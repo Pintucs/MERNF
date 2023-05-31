2ab61c6cb13c0359f5c768f2950c87377cdba685
Pintu kumar <pintucs9695@gmail.com>
2023-05-31 18:51:31 +0530

    online url add

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,72 +1,93 @@
-import React, { useEffect, useState, useCallback } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import UpdateProduct from './UpdateProduct';
 
 const Card = () => {
-  const [product, setProduct] = useState([])
-  const [count, setCount] = useState(1)
-  const [id, setId] = useState(null)
+  const [product, setProduct] = useState([]);
+  const [count, setCount] = useState(1);
+  const [id, setId] = useState(null);
+  const [numCards, setNumCards] = useState(1);
 
-  localStorage.setItem(`${id}`, JSON.stringify({ id }))
-  localStorage.setItem(`count`, JSON.stringify({ count }))
+  localStorage.setItem(`${id}`, JSON.stringify({ id }));
+  localStorage.setItem(`count`, JSON.stringify({ count }));
 
   const getData = useCallback(async () => {
-    const result = await fetch("https://brownstackpd.onrender.com/products");
-    const show = await result.json()
-    setProduct(show)
-  }, [setProduct])
+    const result = await fetch('https://brownstackpd.onrender.com/products');
+    const show = await result.json();
+    setProduct(show);
+  }, [setProduct]);
 
   useEffect(() => {
     getData();
-  }, [getData])
-
+  }, [getData]);
 
   const handleUpdate = useCallback(async () => {
-    const result = await fetch("https://brownstackpd.onrender.com/products");
-    const show = await result.json()
-    setProduct(show)
+    const result = await fetch('https://brownstackpd.onrender.com/products');
+    const show = await result.json();
+    setProduct(show);
     getData();
   }, [getData]);
 
-
-
-
   const searchHandle = async (e) => {
-    let key = e.target.value
+    let key = e.target.value;
     if (key) {
-      let result = await fetch(`https://brownstackpd.onrender.com/search/${key}`)
-      result = await result.json()
+      let result = await fetch(`https://brownstackpd.onrender.com/search/${key}`);
+      result = await result.json();
       if (result) {
-        setProduct(result)
+        setProduct(result);
       }
     } else {
-      getData()
+      getData();
     }
-  }
+  };
 
   const deleteData = async (id) => {
     let result = await fetch(`https://brownstackpd.onrender.com/products/${id}`, {
-      method: "delete"
-    })
-    const ids = await result.json()
+      method: 'delete'
+    });
+    const ids = await result.json();
     if (ids) {
-      getData()
+      getData();
     }
-  }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setNumCards(4);
+      } else {
+        setNumCards(1);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <div className="d-flex justify-content-end">
         <div className="col-md-4 mb-4">
-          <input type="text" onChange={searchHandle} placeholder="Find Product Here..." name="name" className="form-control m-2" />
+          <input
+            type="text"
+            onChange={searchHandle}
+            placeholder="Find Product Here..."
+            name="name"
+            className="form-control m-2"
+          />
         </div>
       </div>
 
       <div className="row">
         {product.length > 0 ? (
           product.map((v, index) => (
-            <div key={index} className="col-md-3 col-sm-6 mb-4">
+            <div key={index} className={`col-md-${12 / numCards} col-sm-6 mb-4`}>
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">{v.name}</h5>
@@ -74,14 +95,23 @@ const Card = () => {
                   <p className="card-text">{v.prize}</p>
                   <p className="card-text"> {v.company}</p>
                   <div className="d-grid gap-2">
-                    <Link to={"/addtocard/" + v._id}>
-                      <Button className="btn btn-primary col-12" onClick={() => {
-                        setCount(count + 1)
-                        setId(v._id)
-                      }}>Add To Cart</Button>
+                    <Link to={'/addtocard/' + v._id}>
+                      <Button
+                        className="btn btn-primary col-12"
+                        onClick={() => {
+                          setCount(count + 1);
+                          setId(v._id);
+                        }}
+                      >
+                        Add To Cart
+                      </Button>
                     </Link>
-                    <Button onClick={() => deleteData(v._id)} className='btn btn-danger'>Delete</Button>
-                    <Button className="btn btn-primary p-0" ><UpdateProduct id={v._id} onUpdate={handleUpdate} /></Button>
+                    <Button onClick={() => deleteData(v._id)} className="btn btn-danger">
+                      Delete
+                    </Button>
+                    <Button className="btn btn-primary p-0">
+                      <UpdateProduct id={v._id} onUpdate={handleUpdate} />
+                    </Button>
                   </div>
                 </div>
               </div>
@@ -92,7 +122,107 @@ const Card = () => {
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Card;
+
+
+// import React, { useEffect, useState, useCallback } from 'react'
+// import { Link } from 'react-router-dom'
+// import { Button } from 'react-bootstrap';
+// import UpdateProduct from './UpdateProduct';
+
+// const Card = () => {
+//   const [product, setProduct] = useState([])
+//   const [count, setCount] = useState(1)
+//   const [id, setId] = useState(null)
+
+//   localStorage.setItem(`${id}`, JSON.stringify({ id }))
+//   localStorage.setItem(`count`, JSON.stringify({ count }))
+
+//   const getData = useCallback(async () => {
+//     const result = await fetch("https://brownstackpd.onrender.com/products");
+//     const show = await result.json()
+//     setProduct(show)
+//   }, [setProduct])
+
+//   useEffect(() => {
+//     getData();
+//   }, [getData])
+
+
+//   const handleUpdate = useCallback(async () => {
+//     const result = await fetch("https://brownstackpd.onrender.com/products");
+//     const show = await result.json()
+//     setProduct(show)
+//     getData();
+//   }, [getData]);
+
+
+
+
+//   const searchHandle = async (e) => {
+//     let key = e.target.value
+//     if (key) {
+//       let result = await fetch(`https://brownstackpd.onrender.com/search/${key}`)
+//       result = await result.json()
+//       if (result) {
+//         setProduct(result)
+//       }
+//     } else {
+//       getData()
+//     }
+//   }
+
+//   const deleteData = async (id) => {
+//     let result = await fetch(`https://brownstackpd.onrender.com/products/${id}`, {
+//       method: "delete"
+//     })
+//     const ids = await result.json()
+//     if (ids) {
+//       getData()
+//     }
+//   }
+
+//   return (
+//     <>
+//       <div className="d-flex justify-content-end">
+//         <div className="col-md-4 mb-4">
+//           <input type="text" onChange={searchHandle} placeholder="Find Product Here..." name="name" className="form-control m-2" />
+//         </div>
+//       </div>
+
+//       <div className="row">
+//         {product.length > 0 ? (
+//           product.map((v, index) => (
+//             <div key={index} className="col-md-3 col-sm-6 mb-4">
+//               <div className="card">
+//                 <div className="card-body">
+//                   <h5 className="card-title">{v.name}</h5>
+//                   <p className="card-text">{v.model}</p>
+//                   <p className="card-text">{v.prize}</p>
+//                   <p className="card-text"> {v.company}</p>
+//                   <div className="d-grid gap-2">
+//                     <Link to={"/addtocard/" + v._id}>
+//                       <Button className="btn btn-primary col-12" onClick={() => {
+//                         setCount(count + 1)
+//                         setId(v._id)
+//                       }}>Add To Cart</Button>
+//                     </Link>
+//                     <Button onClick={() => deleteData(v._id)} className='btn btn-danger'>Delete</Button>
+//                     <Button className="btn btn-primary p-0" ><UpdateProduct id={v._id} onUpdate={handleUpdate} /></Button>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           ))
+//         ) : (
+//           <h1>No Result Found</h1>
+//         )}
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,34 @@ const Main = () => {
         <>
             <Header />
             <h1 className='text-center'>Discover NEW Arrivals</h1>
-                <Card />
-                <Imageslide />
+             <Card />
+             <Imageslide />
         </>
 
     )
 }
 
 export default Main
+// import React from 'react';
+// import Header from './Header';
+// import Card from './Card';
+// import Imageslide from './Imageslide';
+
+// const Main = () => {
+//   return (
+//     <>
+//       <Header />
+//       <div className="container">
+//         <h1 className="text-center mt-4 mb-3">Discover NEW Arrivals</h1>
+//         <div className="row">
+//           <div className="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
+//             <Card />
+//           </div>
+//         </div>
+//         <Imageslide />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Main;
